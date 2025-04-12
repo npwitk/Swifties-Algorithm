c25f3077a3a6243027e205f9cdbc97abcecc53b6
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-04-12 20:48:40 +0700

    Add coin change

---
+++ CoinChangeDP/CoinChangeDP.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+    if amount == 0 { return 0 }
+    var dp = Array(repeating: amount + 1, count: amount + 1)
+    dp[0] = 0
+    for i in 1...amount {
+        for coin in coins {
+            if i >= coin {
+                dp[i] = min(dp[i], dp[i-coin] + 1)
+            }
+        }
+    }
+    return dp[amount] > amount ? -1 : dp[amount]
+}
+
+coinChange([1, 2, 5, 10], 7)
