d7594fd806b1fff27d90892874287fe17e532c05
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-04-27 23:02:39 +0700

    Add RomanToInteger

---
+++ RomanToInteger/RomanToInteger.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+func romanToInt(s: String) -> Int {
+    let dict = initDict()
+    let chars = [Character](s.reversed())
+    var res = 0
+    
+    for i in 0..<chars.count {
+        guard let current = dict[String(chars[i])] else {
+            return res
+        }
+        if i > 0 && current < dict[String(chars[i - 1])]! {
+            res -= current
+        } else {
+            res += current
+        }
+    }
+    
+    return res
+}
+
+private func initDict() -> [String: Int] {
+    var dict = [String: Int]()
+    
+    dict["I"] = 1
+    dict["V"] = 5
+    dict["X"] = 10
+    dict["L"] = 50
+    dict["C"] = 100
+    dict["D"] = 500
+    dict["M"] = 1000
+    
+    return dict
+}
+
+romanToInt(s: "IV")
+romanToInt(s: "MCMLXXXIX")
+
