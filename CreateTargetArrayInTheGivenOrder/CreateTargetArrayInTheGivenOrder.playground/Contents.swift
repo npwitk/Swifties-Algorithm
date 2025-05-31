1a5d455e488c42ec46a290953200db589d37e28c
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-05-31 21:44:57 +0700

    Add Create Target Array in the given order

---
+++ CreateTargetArrayInTheGivenOrder/CreateTargetArrayInTheGivenOrder.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+func createTargetArray(_ nums: [Int], _ index: [Int]) -> [Int] {
+    var target: [Int] = []
+    
+    for i in 0..<nums.count {
+        target.insert(nums[i], at: index[i])
+    }
+    
+    return target
+}
