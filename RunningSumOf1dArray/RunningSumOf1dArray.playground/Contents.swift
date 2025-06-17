a15f0f165390bc5c3d84e3184b64d05165bd6c53
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-06-17 18:07:14 +0700

    Add RunningSumOf1dArray

---
+++ RunningSumOf1dArray/RunningSumOf1dArray.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+func runningSum(_ nums: [Int]) -> [Int] {
+    var result: [Int] = []
+    var sum = 0
+    
+    for num in nums {
+        sum += num
+        result.append(sum)
+    }
+    
+    return result
+}
