4a8feb2fe02bc8207bad25f2481978e752c7cf77
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-08-06 19:48:26 +0700

    Add Find the Index of the First Occurrence in a String

---
+++ FindTheIndexOfTheFirstOccurrenceInAString/FindTheIndexOfTheFirstOccurrenceInAString.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+func strStr(_ haystack: String, _ needle: String) -> Int {
+    if let range = haystack.range(of: needle) {
+        return haystack.distance(from: haystack.startIndex, to: range.lowerBound)
+    } else {
+        return -1
+    }
+}
+
+strStr("sadbutsad", "sad")
