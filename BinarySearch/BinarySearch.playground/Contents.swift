2d0f5d3489a8f3b3e16a775a6894f3cd88f5d700
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-08-12 23:49:44 +0700

    Add more simple version of Binary Search

--- BinarySearch/BinarySearch.playground/Contents.swift
+++ BinarySearch/BinarySearch.playground/Contents.swift
@@ -30,3 +30,25 @@ let binarySearch31 = array.binarySearch(for: 31)
 
 print("firstIndex(of:): \(String(describing: search31))")
 print("binarySearch(for:): \(String(describing: binarySearch31))")
+
+
+// EASIER
+
+func search(_ nums: [Int], _ target: Int) -> Int {
+    var left = 0
+    var right = nums.count - 1
+    
+    while left <= right {
+        let mid = left + (right - left) / 2
+        
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    
+    return -1
+}
